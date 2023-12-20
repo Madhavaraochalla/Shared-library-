6a94b8d92b3dff9a5992515823f15520da2bc509
MADHAV <152585154+Madhavaraochalla@users.noreply.github.com>
2023-12-20 13:12:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,11 +1,9 @@
-def
-    cicd.ConDownload(repo)
+def cicd.ConDownload(repo)
      {
            git "https://github.com/Madhavaraochalla/Maven.git"
      }
          
-def
-  cicd.ConBuild()
+def cicd.ConBuild()
 {
  
 }
