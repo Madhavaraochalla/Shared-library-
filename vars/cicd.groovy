dbfd89d8ac0e3c20b325b9fd5b7ecb1cf0872de2
MADHAV <152585154+Madhavaraochalla@users.noreply.github.com>
2023-12-20 13:29:03 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def ConBuild()
 {
  sh 'mvn package'
 }
+
+def ConDeploy(jobname,ip,appname)
+{
+     deploy adapters: [tomcat9(credentialsId: '0fd54dbb-706f-4ddc-b969-d046bd1a486b', path: '', url: 'http://172.31.4.118:8080')], contextPath: 'testapp', war: '**/*.war'
+}
